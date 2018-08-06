27b788156466ca579776a895c852005f5b81dc5d
pnayana <pnayana@DIN73002980.corp.capgemini.com>
2018-08-06 10:53:00 +0530

    changes in greet method

--- Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
+++ Day1-GitDemo/src/main/java/org/cap/demo/MainClass.java
@@ -3,7 +3,6 @@ package org.cap.demo;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		System.out.println("Hello world");
 		greet();
@@ -13,5 +12,7 @@ public class MainClass {
 	public static void greet()
 	{
 		System.out.println("Hello");
+		System.out.println("Hello1");
+		
 	}
 }
